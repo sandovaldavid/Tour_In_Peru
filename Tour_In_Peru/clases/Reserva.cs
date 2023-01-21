d813a92ed6e60d9cf8173a4e421bc2efc8915c71
Juan David Sandoval Salvador <sandovaldavid2201@gmail.com>
2023-01-21 00:32:17 -0500

    paginas

---
+++ Tour_In_Peru/clases/Reserva.cs
@@ -0,0 +1,28 @@
+﻿
+class Reserva
+{
+    private int idReserva;
+    private DateTime fechaReserva;
+    private int numeroCuenta;
+    private string tipo;
+    private string nombreAgencia;
+    private string estado;
+
+
+    public Reserva(int idReserva, DateTime fechaReserva, int numeroCuenta, string tipo, string nombreAgencia, string estado)
+    {
+        this.idReserva = idReserva;
+        this.fechaReserva = fechaReserva;
+        this.numeroCuenta = numeroCuenta;
+        this.tipo = tipo;
+        this.nombreAgencia = nombreAgencia;
+        this.estado = estado;
+    }
+
+    public int IdReserva { get => idReserva; set => idReserva = value; }
+    public DateTime FechaReserva { get => fechaReserva; set => fechaReserva = value; }
+    public int NumeroCuenta { get => numeroCuenta; set => numeroCuenta = value; }
+    public string Tipo { get => tipo; set => tipo = value; }
+    public string NombreAgencia { get => nombreAgencia; set => nombreAgencia = value; }
+    public string Estado { get => estado; set => estado = value; }
+}
